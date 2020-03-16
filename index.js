4928851a1274628ec7f700670bbd367e5490b5c9
MarinaBuslaeva <51537290+MarinaBuslaeva@users.noreply.github.com>
2020-03-16 09:20:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-<script
-	src="https://code.jquery.com/jquery-3.4.1.js"></script>
 var numbers = document.querySelectorAll(".number");
 var operations = document.querySelectorAll(".operation");
 var comma = document.getElementById("comma");
 var C = document.getElementById("C");
 var CE = document.getElementById("CE");
 var equal = document.getElementById("equal");
-console.log(comma);
 
+for (var i=0; i<numbers.length; i++) {
+	var number = numbers[i];
+	numbers.addEventListener("click"), function(e){
+		console.log("Клик по кнопке с номером");
+};
+}
 
