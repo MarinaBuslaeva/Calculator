32cf7dda71151e0dfeca5a719cebb2b101427421
MarinaBuslaeva <51537290+MarinaBuslaeva@users.noreply.github.com>
2020-03-16 08:55:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 <script
 	src="https://code.jquery.com/jquery-3.4.1.js"></script>
 var numbers = document.querySelectorAll(".number");
-console.log(numbers);
+var operations = document.querySelectorAll(".operation");
+var comma = document.getElementById("comma");
+var C = document.getElementById("C");
+var CE = document.getElementById("CE");
+var equal = document.getElementById("equal");
+console.log(comma);
 
 
