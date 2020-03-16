f094cfe48e49a9efa6f02fa66d058129794932c8
MarinaBuslaeva <51537290+MarinaBuslaeva@users.noreply.github.com>
2020-03-16 09:23:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var numbers = document.querySelectorAll(".number");
+var numbers = document.querySelectorAll('.number');
 var operations = document.querySelectorAll(".operation");
 var comma = document.getElementById("comma");
 var C = document.getElementById("C");
@@ -7,7 +7,7 @@ var equal = document.getElementById("equal");
 
 for (var i=0; i<numbers.length; i++) {
 	var number = numbers[i];
-	numbers.addEventListener("click"), function(e){
+	numbers.addEventListener('click'), function(e){
 		console.log("Клик по кнопке с номером");
 };
 }
